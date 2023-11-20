7c6b19720cafdcdfa2cf37e955785934e6149b6d
Natalia Rubio <nataliav.rubio@gmail.com>
2023-11-20 20:57:49 -0300

    Primer commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Bienvenidos!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D2E3C8',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  texto: {
+    color: '#86A789',
+    fontSize: 25,
   },
 });
